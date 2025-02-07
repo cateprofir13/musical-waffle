6e8a70d7424aa3c2b764fa105c1639857924f3e6
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-07 15:56:06 -0600

    Update commit

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,22 @@
 class Shelter
+    attr_reader :name, :capacity, :pets
+    def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @pets = []
+    end
+
+ def add_pet(pet)
+
+    @pets << pet
+
+ end
+
+def call_pets
+    pets.map do |pet|
+     "#{pet}!"
+    end
+ 
 end
+end
+
